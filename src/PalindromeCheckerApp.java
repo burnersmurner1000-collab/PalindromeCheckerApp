c61b4e1fb5c847174411072b3ac85a2612f90084
burnersmurner <burnersmurner1000@gmail.com>
2026-03-11 15:19:00 +0530

    Implement UC8: Palindrome check using LinkedList

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,17 @@
-import java.util.Deque;
-import java.util.ArrayDeque;
+import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "refer";
-        Deque<Character> deque = new ArrayDeque<>();
+        String input = "level";
+        LinkedList<Character> list = new LinkedList<>();
         for (char c : input.toCharArray()) {
-            deque.addLast(c);
+            list.add(c);
         }
         boolean isPalindrome = true;
-        while (deque.size() > 1) {
-            char front = deque.removeFirst();
-            char rear = deque.removeLast();
-            if (front != rear) {
+        while (list.size() > 1) {
+            char first = list.removeFirst();
+            char last = list.removeLast();
+            if (first != last) {
                 isPalindrome = false;
                 break;
             }
