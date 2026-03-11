277746b207e831a311d9a821aeb069eb59a66d56
burnersmurner <burnersmurner1000@gmail.com>
2026-03-11 15:22:09 +0530

    Implement UC12: Palindrome check using strategy pattern

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,24 +1,48 @@
-public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        String input = "racecar";
-        PalindromeService service = new PalindromeService();
-        boolean result = service.checkPalindrome(input);
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + result);
-    }
-}
+import java.util.*;
 
-class PalindromeService {
-    public boolean checkPalindrome(String input) {
-        int start = 0;
-        int end = input.length() - 1;
-        while (start < end) {
-            if (input.charAt(start) != input.charAt(end)) {
-                return false;
+public class PalindromeCheckerApp {
+    interface PalindromeStrategy {
+        boolean check(String input);
+    }
+
+    static class StackStrategy implements PalindromeStrategy {
+        public boolean check(String input) {
+            Stack<Character> stack = new Stack<>();
+            for (char c : input.toCharArray()) {
+                stack.push(Character.toLowerCase(c));
             }
-            start++;
-            end--;
+            for (char c : input.toCharArray()) {
+                if (Character.toLowerCase(c) != stack.pop()) {
+                    return false;
+                }
+            }
+            return true;
         }
-        return true;
+    }
+
+    static class DequeStrategy implements PalindromeStrategy {
+        public boolean check(String input) {
+            Deque<Character> deque = new ArrayDeque<>();
+            for (char c : input.toCharArray()) {
+                deque.addLast(Character.toLowerCase(c));
+            }
+            while (deque.size() > 1) {
+                if (deque.removeFirst() != deque.removeLast()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        String input = "Level";
+        System.out.println("Input: " + input);
+        PalindromeStrategy stackStrategy = new StackStrategy();
+        boolean stackResult = stackStrategy.check(input);
+        System.out.println("StackStrategy - Is Palindrome? : " + stackResult);
+        PalindromeStrategy dequeStrategy = new DequeStrategy();
+        boolean dequeResult = dequeStrategy.check(input);
+        System.out.println("DequeStrategy - Is Palindrome? : " + dequeResult);
     }
 }
