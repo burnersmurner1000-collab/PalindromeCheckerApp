4e06873ff3efe88eedc2cdb2d3687b890d8d7963
burnersmurner <burnersmurner1000@gmail.com>
2026-03-11 16:24:57 +0530

    Implement UC4: Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,27 +1,10 @@
- * @version 4.0
- */
-public class UseCase4PalindromeCheckerApp {
-
-    /**
-     * Application entry point for UC4.
-     *
-     * @param args Command-line arguments
-     */
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        // Declare and initialize the input string
         String input = "radar";
-
-        // Convert the string into a character array
         char[] chars = input.toCharArray();
-
-        // Initialize pointers
         int start = 0;
         int end = chars.length - 1;
-
-        // Assume palindrome initially
         boolean isPalindrome = true;
-
-        // Continue comparison until pointers cross
         while (start < end) {
             if (chars[start] != chars[end]) {
                 isPalindrome = false;
@@ -30,8 +13,6 @@ public class UseCase4PalindromeCheckerApp {
             start++;
             end--;
         }
-
-        // Display result
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
